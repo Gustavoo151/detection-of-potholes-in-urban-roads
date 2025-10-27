f56af244fffeb03498faf8a31cbb1685cfa3f8e9
gustavoo151 <josegustavooliveira121@gmail.com>
2025-10-26 21:49:11 -0300

    Remove old repositore

--- sensorBoard/STM32CubeIDE/Debug/Drivers/BSP/Components/lis2duxs12_reg.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/lis2duxs12_reg.o: \
- C:/Users/jpaul/Documents/STM/IKS4A1/Sensor_Board_Dataset/Drivers/BSP/Components/lis2duxs12/lis2duxs12_reg.c \
- C:/Users/jpaul/Documents/STM/IKS4A1/Sensor_Board_Dataset/Drivers/BSP/Components/lis2duxs12/lis2duxs12_reg.h
-C:/Users/jpaul/Documents/STM/IKS4A1/Sensor_Board_Dataset/Drivers/BSP/Components/lis2duxs12/lis2duxs12_reg.h:
